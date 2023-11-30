969f09782e64dfdeb1196118e6f731f02a060450
Sourav Kumar <souravk326@gmail.com>
2023-11-30 10:33:14 +0000

    simple todo api

---
+++ Golang-simple-app/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//todo struct
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+//sample todos
+
+var todos = []todo{
+	{
+		ID:        "1",
+		Item:      "Clean Room",
+		Completed: false,
+	},
+	{
+		ID:        "2",
+		Item:      "Read Book",
+		Completed: false,
+	},
+	{
+		ID:        "3",
+		Item:      "Record Video",
+		Completed: false,
+	},
+}
+
+//get todos
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+//add todo
+
+func addTodo(context *gin.Context) {
+	var newTodo todo
+
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+
+	context.IndentedJSON(http.StatusCreated, newTodo)
+}
+
+//get todo by id
+
+func getTodoById(id string) (*todo, error) {
+	for i, todo := range todos {
+		if todo.ID == id {
+			return &todos[i], nil
+		}
+	}
+	return nil, errors.New("Todo not found.")
+}
+
+//get todo by id
+
+func getTodo(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found!"})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
+//toggle todo status
+
+func toggleTodoStatus(context *gin.Context) {
+	id := context.Param("id")
+	todo, err := getTodoById(id)
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found!"})
+		return
+	}
+
+	todo.Completed = !todo.Completed
+
+	context.IndentedJSON(http.StatusOK, todo)
+}
+
+//main function
+
+func main() {
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.GET("/todos/:id", getTodo)
+	router.PATCH("/todos/:id", toggleTodoStatus)
+	router.POST("/todos", addTodo)
+	router.Run("localhost:9090")
+}
